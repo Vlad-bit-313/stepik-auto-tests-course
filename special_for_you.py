c642d6a8c7cb865e49c2e06678844023d000b22d
Vlad-bit-313 <vladia313@gmail.com>
2021-06-26 15:38:13 +0700

    Программа выводит 3 числа которые ввели с клавиатуры: max, min и то что осталось

---
+++ special_for_you.py
@@ -0,0 +1,31 @@
+# put your python code here
+a = int(input())
+b = int(input())
+c = int(input())
+
+if b <= a >= c:
+    print(a)
+elif a <= b >= c:
+    print(b)
+elif a <= c >= b:
+    print(c)
+
+# находим минимум
+if (b >= a and a <= c):
+    print(a)
+elif (a >= b and b <= c):
+    print(b)
+elif (a >= c and c <= b):
+    print(c)
+
+# находим второе число
+if (b <= a >= c) and (a >= b <= c):
+    print(c)
+elif (b >= a <= c) and (a <= b >= c):
+    print(c)
+elif (a <= b >= c) and (a >= c <= b):
+    print(a)
+elif (a >= b <= c) and (a <= c >= b):
+    print(a)
+else:
+    print(b)
