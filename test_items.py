5c10cda240a172f872a28cc0a6aac9a06844c3c3
Vlad-bit-313 <vladia313@gmail.com>
2021-06-29 00:02:53 +0700

    Assert теперь работет стабильно

--- test_items.py
+++ test_items.py
@@ -4,6 +4,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_button_add_cart(browser):
     browser.get(link)
-    time.sleep(30)
-    cart_button = browser.find_element_by_css_selector("[type='submit'].btn-add-to-basket")
+    time.sleep(5)
+    cart_button = browser.find_elements_by_css_selector("[type='submit'].btn-add-to-basket")
     assert cart_button, "Should be a button!"
