568548122cbcf4358185abc5e7be2dde74bcfa88
Vlad-bit-313 <vladia313@gmail.com>
2021-06-28 22:17:07 +0700

    Изменил название функции

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@
 import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_button_add_(browser):
+def test_button_add_cart(browser):
     browser.get(link)
     time.sleep(30)
     cart_button = browser.find_element_by_css_selector("[type='submit'].btn-add-to-basket")
